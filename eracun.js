56f46874250371442f7eb4c1a96a53a824e00e3a
David Rubin <david.rubin95@gmail.com>
2016-05-07 15:28:13 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -211,11 +211,20 @@ streznik.post('/prijava', function(zahteva, odgovor) {
       //TODO: add fields and finalize
       //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
       //stmt.finalize();
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3);
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
-  
-    odgovor.end();
+    var odziv = "Stranka je bila uspešno registrirana.";
+    
+    vrniStranke(function(napaka3, stranke) {
+      vrniRacune(function(napaka4, racuni) {
+        if (napaka2)
+          odziv = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+        odgovor.render('prijava', {sporocilo: odziv, seznamStrank: stranke, seznamRacunov: racuni});  
+      }) 
+    });
   });
 })
 
